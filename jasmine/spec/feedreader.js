df43d4cbb8fb6216b1bbe29fe8b4a7ad719d67f9
Archisman Sahoo <archismansahoo8@gmail.com>
2016-11-24 17:41:09 +0530

    Edited 3 specs
    1. Corrected name spec
    2. Included correct jQuery method in Initial Entries spec
    3. Modified New feed selection spec's async call

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,4 +1,3 @@
-
 $(function() {
 
     describe('RSS Feeds', function() {
@@ -10,67 +9,79 @@ $(function() {
 
         //Checks all url properties are defined and not empty
         it('have URLs', function() {
-            allFeeds.forEach(function(allFeeds){
+            allFeeds.forEach(function(allFeeds) {
                 expect(allFeeds.url).toBeDefined();
                 expect(allFeeds.url.length).not.toBe(0);
-           });
-         });
-
-         //Checks all name properties are defined and not empty
-          it('have names', function() {
-            allFeeds.forEach(function(allFeeds){
-                expect(allFeeds.url).toBeDefined();
-                expect(allFeeds.url.length).not.toBe(0);
-           });
-         });
+            });
+        });
+        //Checks all name properties are defined and not empty
+        it('have names', function() {
+            allFeeds.forEach(function(allFeeds) {
+                expect(allFeeds.name).toBeDefined();
+                expect(allFeeds.name.length).not.toBe(0);
+            });
+        });;
     });
 
 
     //New suite named "The menu"
     describe('The menu', function() {
 
-         //Checks menu element is hidden by default or not
-         it('is hidden by default', function(){
-           expect($('body').hasClass('menu-hidden')).toBe(true);
-         });
+        //Checks menu element is hidden by default or not
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-          //Checks menu element is visible when clicked and hidden when clicked again or not
-          it('is visible when clicked and hidden when clicked again', function(){
+        //Checks menu element is visible when clicked and hidden when clicked again or not
+        it('is visible when clicked and hidden when clicked again', function() {
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
         });
+    });
 
     //New suite named "Initial Entries"
     describe('Initial Entries', function() {
 
-         //Calls the loadFeed function
-         beforeEach(function(done){
-           loadFeed(0,done);
-         });
-         //Checks at least 1 entry element is present inside feed container or not
-         it('has at least single .entry element within .feed container', function(done) {
-           expect($('.feed').children().length).not.toBe(0);
-           done();
-         });
-       });
+        //Calls the loadFeed function
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+        //Checks at least 1 entry element is present inside feed container or not
+        it('has at least single .entry element within .feed container', function() {
+            //No of feeds
+            console.log($('.feed .entry').length);
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+        });
+    });
 
     //New suite named "New Feed Selection"
     describe('New Feed Selection', function() {
 
-         var feedContents;
-         //Calls the loadFeed function with 2nd allFeeds property
-         beforeEach(function(done) {
-            loadFeed(1, done);
-            feedContents = $('.feed').html();
-          });
+        var contentFeed;
+
+        //Calls the loadFeed function with 4th allFeeds property
+        beforeEach(function(done) {
+                loadFeed(3, function() {
+                    contentFeed = $('.feed').html();
+                    done();
+                });
+            })
+            // feedContents = $('.feed').html();
+
         //Checks new feed has been loaded or not
         it('is loaded by the loadFeed function', function(done) {
-          loadFeed(0);
-          expect($('.feed').html()).not.toEqual(feedContents);
-          done();
+            //If make it to equal with the above
+            //loadFeed function's 1st argument, then
+            //spec fails
+            loadFeed(2, function() {
+                //Checks if particular feed's content is empty or not
+                console.log($('.feed').find('h2')[2].textContent);
+                //Compare 3rd and 4th allFeeds's property html content
+                expect($('.feed').html()).not.toEqual(contentFeed);
+                done();
+            });
         });
-        });
+    });
 }());
